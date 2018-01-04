8b8ff7af8d6349f86f61e17250dacf3255520a98
Igor Souza Martins <nulldreams@gmail.com>
2018-01-04 11:11:42 -0200

    feat: refatoração, novas funções

--- example/index.js
+++ example/index.js
@@ -1,14 +1,38 @@
 const nfe2stalk = require('../lib/nfe2stalk')
 
 let options = {
-  every: '10',
-  fullNotify: true
+  tempo: '10',
+  tipoTempo: 's',
+  notificacaoCompleta: true
 }
 
-nfe2stalk.monitor.iniciar(options)
+nfe2stalk.monitor.iti.iniciar(options)
+nfe2stalk.monitor.sefaz.iniciar(options)
 
-nfe2stalk.monitor.monitorar.on('alerta', (alerta) => {
-  console.log('alerta')
-  console.log(alerta)
+nfe2stalk.monitor.iti.monitorar.on('alerta', (iti) => {
+  console.log('iti')
+  console.log(iti)
   console.log('------------------------------------')
+})
+
+nfe2stalk.monitor.sefaz.monitorar.on('alerta', (sefaz) => {
+  console.log('sefaz')
+  console.log(sefaz)
+  console.log('------------------------------------')
+})
+
+// nova versão
+
+nfe2stalk.monitor.iti.limparAlertas().then((qtAlertas) => {
+  console.log(`${qtAlertas} removidos`)
+})
+.catch((err) => {
+  console.log(err)
+})
+
+nfe2stalk.monitor.sefaz.limparAlertas().then((qtAlertas) => {
+  console.log(`${qtAlertas} removidos`)
+})
+.catch((err) => {
+  console.log(err)
 })
